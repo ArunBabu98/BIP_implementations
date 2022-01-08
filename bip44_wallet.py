7bbca4a49027f9caae97a373b4b2763d22195b06
ArunBabu98 <arun.b@changeangel.io>
2022-01-08 23:41:05 +0530

    Bip-32 key derivation

--- bip44_wallet.py
+++ bip44_wallet.py
@@ -2,6 +2,12 @@ import os
 import binascii
 import hashlib
 import unicodedata
+import hmac
+import struct
+import ecdsa
+from base58 import b58encode
+from ecdsa.curves import SECP256k1
+from ecdsa.ecdsa import int_to_string, string_to_int
 
 bits = 256
 print("Bytes = " + str(bits//8))
@@ -46,4 +52,52 @@ mnemonic = normalized_mnemonic.encode("utf-8")
 passphrase = passphrase.encode("utf-8")
 
 bin_seed = hashlib.pbkdf2_hmac("sha512", mnemonic, passphrase, 2048)
-print(binascii.hexlify(bin_seed[:64]))
+
+
+# BIP-39 seed to BIP-32 Master Root Key
+
+seed = binascii.hexlify(bin_seed[:64])
+# seed = binascii.unhexlify("000102030405060708090a0b0c0d0e0f")
+decoded_seed = seed.decode("utf-8")
+final_seed = binascii.unhexlify(decoded_seed)
+print(decoded_seed)
+I = hmac.new(b"Bitcoin seed", final_seed, hashlib.sha512).digest()
+Il, Ir = I[:32], I[32:]
+
+secret = Il
+chain = Ir
+
+xprv = binascii.unhexlify("0488ade4")
+xpub = binascii.unhexlify("0488b21e")
+depth = b"\x00"
+fpr = b'\0\0\0\0'
+index = 0
+child = struct.pack('>L', index) 
+
+k_priv = ecdsa.SigningKey.from_string(secret, curve=SECP256k1)
+K_priv = k_priv.get_verifying_key()
+
+data_priv = b'\x00' + (k_priv.to_string())  # ser256(p): serializes integer p as a 32-byte sequence
+
+# serialization the coordinate pair P = (x,y) as a byte sequence using SEC1's compressed form
+if K_priv.pubkey.point.y() & 1:
+    data_pub= b'\3'+int_to_string(K_priv.pubkey.point.x())
+else:
+    data_pub = b'\2'+int_to_string(K_priv.pubkey.point.x())
+
+raw_priv = xprv + depth + fpr + child + chain + data_priv
+raw_pub = xpub + depth + fpr + child + chain + data_pub
+
+# Double hash using SHA256
+hashed_xprv = hashlib.sha256(raw_priv).digest()
+hashed_xprv = hashlib.sha256(hashed_xprv).digest()
+hashed_xpub = hashlib.sha256(raw_pub).digest()
+hashed_xpub = hashlib.sha256(hashed_xpub).digest()
+
+# Append 4 bytes of checksum
+raw_priv += hashed_xprv[:4]
+raw_pub += hashed_xpub[:4]
+
+# Return base58
+print(b58encode(raw_priv))
+print(b58encode(raw_pub))
